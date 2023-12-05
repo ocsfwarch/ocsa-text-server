08a131f268e4be619c99c0b2eef3e44a51ec56ff
ocsfwarch <tpartcal@gmail.com>
2023-12-05 10:46:05 -0800

    Adds cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const errorHandler = require('./errors/errorHandler')
 const notFound = require('./errors/notFound')
 const testRouter = require('./components/TestRoute/test.router')
 
+app.use(cors())
 app.use(express.json())
 
 app.use("/test", testRouter)

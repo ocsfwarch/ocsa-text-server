3da6352b8c780dadf9213a627d163cd9a72ea18a
ocsfwarch <tpartcal@gmail.com>
2023-12-07 11:23:18 -0800

    Add date conversion routine

--- src/utils/DateTimeUtils.js
+++ src/utils/DateTimeUtils.js
@@ -5,6 +5,27 @@
  */
 function getLocalDate() {
     const date = new Date()
+    const test = new Intl.DateTimeFormat('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    ).format(date)
+
+    console.log(`Current = ${test}, New = ${getLocalDateFromString(test)}`)
+
+    return new Intl.DateTimeFormat('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    ).format(date)
+}
+
+function getLocalDateFromString(dateString) {
+    const date = new Date(dateString)
 
     return new Intl.DateTimeFormat('en-US', {
         weekday: 'long',
